0383453e8743bbe72f86d98ea6299a5beac8ae8c
GabrielFeodorov <gabriel.feodorov@oracle.com>
2024-09-03 11:21:37 +0300

    corrected output error

--- modules/object_storage/outputs.tf
+++ modules/object_storage/outputs.tf
@@ -1,5 +1,5 @@
 output bucket_name {
-    value = useExistingBucket? null : oci_objectstorage_bucket.these[0].name
+    value = useExistingBucket ? null : oci_objectstorage_bucket.these.0.name
 }
 
 
